cc825f859408b3631021ebe81f44c9841fa6b954
Isaac NoEnemies <gbewaisaac@yahoo.com>
2022-10-05 11:46:06 +0100

    classwork

---
+++ classwork.py
@@ -0,0 +1,21 @@
+# how many seconds are there in 42 minutes 42 seconds 
+# there are 60 seconds in a minute 
+
+min = 42
+secs = 42
+
+# 60 seconds in 1 minute
+t_mins = min * 60
+t_secs = t_mins + secs
+
+print (t_secs)
+
+# how many miles are there in 10 kilometers
+# there are 1.61 kilometers in a mile
+
+kilometers = 1.61
+miles = 10
+total = miles / kilometers
+
+print(total )
+
