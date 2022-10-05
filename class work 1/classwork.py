ed6e5294113160923c274c7f5311de7654201b8c
Isaac NoEnemies <gbewaisaac@yahoo.com>
2022-10-05 14:36:28 +0100

    classwork correction

---
+++ class work 1/classwork.py
@@ -0,0 +1,56 @@
+# how many seconds are there in 42 minutes 42 seconds 
+# there are 60 seconds in a minute 
+
+min = 42
+secs = 42
+
+# 60 seconds in 1 minute
+t_mins = min * 60
+t_secs = t_mins + secs
+
+print (t_secs)
+
+# how many miles are there in 10 kilometers
+# there are 1.61 kilometers in a mile
+
+kilometers = 1.61
+miles = 10
+total = miles / kilometers
+
+print(total )
+
+# if you run a 10 kilometer race in 42 minutes 42 seconds what is your average pace (time per mile in minute and seconds? ) what is your average speed in miles per hour 
+
+# where average speed is total  distance divided by time
+
+distance = 10 
+time = 42_42 
+
+minute = 42 + (42/60)
+42 + 0.7
+m = 42.7 
+
+s = (42*60) + 42
+s = 2562
+print (s)
+
+# where miles per hour =
+m = 6.21/42.7
+m = 0.145
+print(m)
+
+# miles pers second =
+s = 6.21/2562
+s = 0.0024
+print(s)
+
+#miles per hour 
+h = 42.7 / 60
+print (h)
+h = 0.71
+h = 6.21/0.71
+print (h)
+h = 8.74 
+
+#######
+
